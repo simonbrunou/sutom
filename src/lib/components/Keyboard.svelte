f290c2edec18365d452efe863b0589a1add47f2d
Simon <5848809+simonbrunou@users.noreply.github.com>
2026-04-16 14:04:59 +0200

    Update Keyboard.svelte

--- src/lib/components/Keyboard.svelte
+++ src/lib/components/Keyboard.svelte
@@ -13,7 +13,7 @@
 	const rows = [
 		['A', 'Z', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
 		['Q', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M'],
-		['.', 'W', 'X', 'C', 'V', 'B', 'N', 'ENTER', 'BACK']
+		['.', 'W', 'X', 'C', 'V', 'B', 'N', 'BACK', 'ENTER']
 	];
 
 	function getKeyLabel(key: string): string {
